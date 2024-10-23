46ab9b66ab5aad7c85d771f16975ac6ddd52bd91
missk <rostock.vasiljev@gmail.com>
2024-10-23 19:37:21 +0300

    add solution

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -5,8 +5,8 @@ import './Welcome.css';
 const Welcome = () => {
   return (
     <section className="welcome">
-    <span className="welcome__text">Sticky Header!</span>
-  </section>
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
   );
 };
 
